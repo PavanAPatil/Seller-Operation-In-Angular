8dcfbfcc6e117076dedfdc70c9743c70f4318897
PavanAPatil <87060675+PavanAPatil@users.noreply.github.com>
2024-05-02 10:45:49 +0530

    Add files via upload

---
+++ order-list.ts
@@ -0,0 +1,48 @@
+export class orderDB {
+    static list_order = [
+        {
+            "orderId": 15,
+            "mobileNo": 9405009082,
+            "paymentMethod": "COD",
+            "deliveryDate": "06-07-2020",
+            "orderStatus": "<span class='badge badge-primary'>New</span>",
+            "totalAmount":"<span class='fa fa-inr'>150</span>",
+            "createdDatetime": "05-07-2020 18.19 PM",
+            
+           
+        },
+        {
+            "orderId": 16,
+            "mobileNo": 123456789,
+            "paymentMethod": "COD",
+            "deliveryDate": "06-07-2020",
+            "orderStatus": "<span class='badge badge-warning'>Shipped</span>",
+            "totalAmount":"<span class='fa fa-inr'>150</span>",
+            "createdDatetime": "05-07-2020 18.19 PM",
+            
+           
+        },
+        {
+            "orderId": 17,
+            "mobileNo": 9876543210,
+            "paymentMethod": "COD",
+            "deliveryDate": "06-07-2020",
+            "orderStatus": "<span class='badge badge-success'>Delivered</span>",
+            "totalAmount":"<span class='fa fa-inr'>150</span>",
+            "createdDatetime": "05-07-2020 18.19 PM",
+            
+           
+        },
+        {
+            "orderId": 18,
+            "mobileNo": 9405009082,
+            "paymentMethod": "COD",
+            "deliveryDate": "06-07-2020",
+            "orderStatus": "<span class='badge badge-danger'>Canceled</span>",
+            "totalAmount":"<span class='fa fa-inr'>150</span>",
+            "createdDatetime": "05-07-2020 18.19 PM",
+            
+           
+        },
+    ]
+}
